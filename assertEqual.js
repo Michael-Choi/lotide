c209e301d1096dec90005106d9ad47accd0e3f06
Michael-Choi <Michael.choi@queensu.ca>
2019-08-19 17:31:42 +0000

    added assert and head functions

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯  Assertion passed: "${actual} === ${expected}"`);
+    return true;
+  } else {
+    console.log(`Assertion failed: "${actual} !== ${expected}"`);
+    return false;
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
