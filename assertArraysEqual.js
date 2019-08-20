5e2de45d5e16c3b8ec4ac428e4973a0e5cbf867b
Michael-Choi <Michael.choi@queensu.ca>
2019-08-20 17:57:08 +0000

    Added arrays equal assert function

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`💯  Assertion passed: "${actual} === ${expected}"`);
+    return true;
+  } else {
+    console.log(`Assertion failed: "${actual} !== ${expected}"`);
+    return false;
+  }
+};
+const eqArrays = (actual, expected) => {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+// TEST CODE
+assertArrayEqual([1, 2, 3], [1, 2, 3]); // => should PASS
